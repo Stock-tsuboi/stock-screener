6c17217d54cdbb2f8f0e86889f0b2b77023641a2
Stock-tsuboi <softbankaquosphone203sh@gmail.com>
2026-03-26 21:27:19 +0900

    Update screening_ai.py

--- screening_ai.py
+++ screening_ai.py
@@ -571,7 +571,7 @@ def strongest_ai_ranking(model, feature_cols, all_data, feature_data):
 # Step15　パラメータ設定
 # =========================================================
 
-BEST_TH = 0.45　#数値を調整する
+BEST_TH = 0.45  # 数値を調整する
 EXCLUDE_CODES = []
 
 
