b05c6f462917a8cd7b1cdd6388bc09850d7323b3
Stock-tsuboi <softbankaquosphone203sh@gmail.com>
2026-04-14 21:39:25 +0900

    Update screening_ai.py

--- screening_ai.py
+++ screening_ai.py
@@ -1776,7 +1776,8 @@ def run_screening():
     # ★ここが最重要：期待値でソート
 
     # 旧AIが0の銘柄は除外（精度強化）
-    df_merge = df_merge[df_merge["旧AI確率"] > 0]
+    # ★一時的に無効化（デバッグ用）
+    # df_merge = df_merge[df_merge["旧AI確率"] > 0]
     
     df_merge = df_merge.sort_values(
         "期待値",
